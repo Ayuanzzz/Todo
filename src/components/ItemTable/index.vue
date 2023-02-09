3dd926f1caafbac9a4e285ca90c86ebf82937805
Ayuanzzz <1052833858@qq.com>
2023-02-10 07:22:01 +0800

    ItemTable

---
+++ src/components/ItemTable/index.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Check, Delete } from '@element-plus/icons-vue'
+const tableData = [{
+    id: 1,
+    name: 'huhuhuhu'
+}, {
+    id: 2,
+    name: '蝴蝶飞的合法化'
+}]
+</script>
+
+<template>
+    <el-table :data="tableData" style="width: 100%">
+        <el-table-column label="名称" prop="name" style="width: 90%">
+        </el-table-column>
+        <el-table-column fixed="right" label="编辑" width="100">
+            <template #default>
+                <el-button type="success" :icon="Check" circle />
+                <el-button type="danger" :icon="Delete" circle />
+            </template>
+        </el-table-column>
+    </el-table>
+</template>
