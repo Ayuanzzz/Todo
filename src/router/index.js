8a4c3f440aa66636a12571c09a7e093dc25167a3
Ayuanzzz <1052833858@qq.com>
2023-02-21 14:58:22 +0800

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -14,12 +14,21 @@ const router = createRouter({
     },
     {
         path: '/itemtable',
-        component: () => import('@/views/itemTable/index.vue'),
+        component: () => import('../views/itemTable/index.vue'),
     },
     {
         path: '/setItem',
         component: () => import('@/views/setItem/index.vue'),
     }]
+})
+
+// 全局路由守卫
+router.beforeEach((to, from, next) => {
+    console.log(to, from)
+    // if (to.meta.title) {
+    //   document.title = `${to.meta.title}`;
+    // }
+    next()
 })
 
 export default router
